2b54741ddd618b7408f1bbe02553072013f9ee2c
mdemiguels <miguelschz1997@gmail.com>
2024-09-27 13:04:29 +0200

    refactor div grid 16x16 with js

--- script.js
+++ script.js
@@ -2,17 +2,11 @@ const gridContainer = document.querySelector('.container');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 256; i++) {
         
-        const divRow = document.createElement('DIV');
-        gridContainer.appendChild(divRow);
-
-        for (let j = 0; j < 16; j++) {
-            
-            const divColumn = document.createElement('DIV');
-            divRow.appendChild(divColumn);
-
-        }
+        const divPixel = document.createElement('DIV');
+        divPixel.classList = 'pixel';
+        gridContainer.appendChild(divPixel);
         
     }
 });
